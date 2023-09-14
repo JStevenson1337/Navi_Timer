ceee323b6bf25a64f96402533fd60e90039cc327
jeremy stevenson <jeremy.m.stevenson@gmail.com>
2023-09-14 14:50:27 -0500

    Update app_frame.py

--- app_frame.py
+++ app_frame.py
@@ -7,18 +7,26 @@ class Application(tk.Frame):
         tk.Frame.__init__(self, master)
         self.grid()
         self.create_widgets()
+        self.create_buttons()
 
     def create_widgets(self):
         '''Creates the widgets for the application'''
         self.quit_button = tk.Button(self, text='Quit',
             command=self.quit)
-        self.quit_button.grid()
+        self.quit_button.grid(column=4, row=4)
+
+    def create_buttons(self):
+        '''Creates the buttons for the application'''
         self.start_button = tk.Button(self, text='Start',fg='black', bg='green', font=("Helvetica", 20))
-        self.start_button.grid()
+        self.start_button.grid(column=0, row=4)
+        self.pause_button = tk.Button(self, text='Pause/Resume',fg='black', bg='blue', font=("Helvetica", 20))
+        self.pause_button.grid(column=2, row=4)
+        self.stop_button = tk.Button(self, text='Stop',fg='black', bg='red', font=("Helvetica", 20))
+        self.stop_button.grid(column=3, row=4)
 
 app = Application()
 app.master.title('Timer Application')
-app.master.geometry('800x800+10+20')
+# app.master.geometry('800x800+10+20')
 main_label = tk.Label(app, text='Timer', font=('Helvetica', 30))
 main_label.place(x=400, y=20)
 app.mainloop()
