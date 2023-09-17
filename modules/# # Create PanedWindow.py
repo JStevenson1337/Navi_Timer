badf9b6805414d8300796c79fc1c49fdc62734aa
jeremy stevenson <jmstevenson918@gmail.com>
2023-09-16 22:58:37 -0500

    more testing

---
+++ modules/# # Create PanedWindow.py
@@ -0,0 +1,11 @@
+        # # Create PanedWindow
+        # def create_pw(self):
+        #     '''Creates the PanedWindow for the application'''
+        #     pw = PanedWindow(self, orient=VERTICAL)
+        #     pw.pack(fill=BOTH, expand=1)
+
+        #     top = Label(pw, text="top pane")
+        #     pw.add(top)
+
+        #     bottom = Label(pw, text="bottom pane")
+        #     pw.add(bottom)
